1e1343fffdda4503e370e6bb3c851b39b9e5d9b4
Alex Fedotov <alxfed@gmail.com>
2023-01-20 20:41:30 -0600

    empty test

---
+++ tests/test_car.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+
+
+class CarTest:
+    def test_say_state(self):
+        assert False
+
+    def test_accelerate(self):
+        assert False
+
+    def test_brake(self):
+        assert False
+
+    def test_step(self):
+        assert False
+
+    def test_average_speed(self):
+        assert False
